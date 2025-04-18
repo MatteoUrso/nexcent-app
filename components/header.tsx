1fdcf6d7f147539696194291b69628b5a4fd9bb8
INFOR\matteou <m.urso@esaedro.com>
2025-04-18 16:27:56 +0200

    Refactor Header component to use custom Button component and update styles; add Button component with variant support

--- components/header.tsx
+++ components/header.tsx
@@ -1,16 +1,21 @@
 import { Icons } from "./icons";
 import { NavBar } from "./nav-bar";
+import { Button } from "@/components/ui/button";
 
 export function Header() {
   return (
-    <header className="bg-tint-t5 h-[84px] w-full">
+    <header className="bg-silver h-[84px] w-full">
       <div className="container mx-auto flex h-full items-center justify-between px-4 md:px-0">
         <Icons.logo className="hidden md:block" />
         <Icons.icon className="block md:hidden" />
         <NavBar />
-        <div className="flex gap-3.5">
-          <button>Login</button>
-          <button>Sign Up</button>
+        <div className="flex items-center gap-3.5">
+          <Button variant="link" className="h-10">
+            Login
+          </Button>
+          <Button className="h-10 rounded-[6px] px-5 py-2.5 text-sm leading-5 font-medium tracking-normal">
+            Sign up
+          </Button>
         </div>
       </div>
     </header>
---
+++ components/ui/button.tsx
@@ -0,0 +1,46 @@
+import { cn } from "@/lib/utils";
+import { Slot } from "@radix-ui/react-slot";
+import { type VariantProps, cva } from "class-variance-authority";
+import * as React from "react";
+
+const buttonVariants = cva(
+  "text-lg leading-6 font-normal inline-flex items-center justify-center gap-x-2 rounded-sm whitespace-nowrap transition-colors disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:size-6 [&_svg]:shrink-0",
+  {
+    variants: {
+      variant: {
+        default:
+          "bg-primary text-white hover:bg-primary focus-visible:ring-d-grey focus-visible:ring-2 focus-visible:outline-none active:bg-shade-s2 disabled:bg-grey-blue disabled:text-silver",
+        link: "bg-transparent text-primary hover:underline focus-visible:underline focus-visible:ring-d-grey focus-visible:ring-2 focus-visible:outline-none active:text-shade-s2 active:underline disabled:text-silver",
+      },
+      size: {
+        default: "px-8 py-4 h-14",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+);
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
+}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : "button";
+    return (
+      <Comp
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+Button.displayName = "Button";
+
+export { Button, buttonVariants };
