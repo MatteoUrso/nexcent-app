904314b8a55d2f763e005d6417cffc26e9f19225
UfoTheKing <matteourso100@gmail.com>
2025-04-23 15:41:51 +0200

    Add Footer component with copyright information and logo

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,3 +1,20 @@
+import { Icons } from "./icons";
+
 export function Footer() {
-  return <footer className="h-[328px] w-full bg-black"></footer>;
+  return (
+    <footer className="flex h-[328px] w-full items-center bg-black">
+      <div className="container mx-auto flex flex-col-reverse lg:flex-row lg:gap-x-[125px]">
+        <div className="flex flex-col items-center lg:items-start">
+          <Icons.lotoWhite />
+          <p className="text-body-regular-3 text-silver mt-10">
+            Copyright © 2020 Nexcent ltd.
+          </p>
+          <p className="text-body-regular-3 text-silver mt-2">
+            All rights reserved
+          </p>
+        </div>
+        <div></div>
+      </div>
+    </footer>
+  );
 }
